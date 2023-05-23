90db3b27d26d0a66ee575e78cea8ddc74b9e5746
varti21 <77670955+varti21@users.noreply.github.com>
2023-05-24 00:01:17 +0530

    Add files via upload

---
+++ birdshoot/Bird_shoot.js
@@ -0,0 +1,21 @@
+/**Second Version***
+KILL THE BIRDS PURE CSS GAME - NO JS
+------------------------------
+Author: Elad Shechter
+https://twitter.com/eladsc
+https://il.linkedin.com/in/eladshechter/
+https://medium.com/@elad
+
+
+MY FACEBOOK GROUP
+https://www.facebook.com/groups/css.master/
+
+
+The Bird taken from:
+-------------------------------
+https://codepen.io/fixcl/pen/KhAqa
+
+Author: Marco Barría 
+https://twitter.com/marco_bf
+
+******/
